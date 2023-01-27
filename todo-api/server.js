9491de7ed37b1246621180fe6e624e0c403a7b02
Mike Wittenauer <mikewittenauer@me.com>
2023-01-27 07:47:17 -0500

    Move api code to it's own directory

---
+++ todo-api/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/test', (req, res) => res.json({ msg: 'Test Passed' }) );
+
+app.listen(4000, () => {
+    console.log('App listening on port 4000');
+});
